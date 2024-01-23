2dd7fca06d7e1f5485072353490b340e63125984
ruanPcosta <querogold12@gmail.com>
2024-01-22 22:58:25 -0300

    Projeto Python + Django commit2

--- study_async_ruan/settings.py
+++ study_async_ruan/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8rsxgk-w)eysnwb!p9^9z8q!qifh+82w2s9u&@i!e(r&&g%gnb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [127.0.0.1]
 
 
 # Application definition
